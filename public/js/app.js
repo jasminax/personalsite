103e988ce5ca29583c3cf2a6f67de0c8b264ff89
Jasmina Xie <jasmina.xie@gmail.com>
2013-04-21 15:45:07 -0700

    New jQuery: toggle and animate

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,28 @@
 $(document).ready(function(){
-	//alert("Hello Jasmina!");
-})
+
+	// $('.jumbotron h1').hover(function(){
+	// 	$(this).animate({paddingBottom: '+=15px'}, 200);
+	// }, function(){
+	// 	$(this).animate({paddingBottom: '-=15px'}, 200);
+	// });
+
+	$('#food > div').hide();
+
+	// $('#food > div:first').show();
+
+	$('#food h5').click(function(){
+		$(this).next().animate(
+			{'height':'toggle'}, 'slow')
+	});
+
+	$('#toggleButton').click(function(){
+		$('#simrunn').toggle();
+
+		if($('#simrunn').is(':visible')) {
+			$(this).val("Oh");
+		} else{
+			$(this).val("Wat");
+		}
+	});
+
+});
